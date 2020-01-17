45be4fe1e81d87c8e6df85574ffeebf0000514c0
Karan <Kumarkay.ca@gmail.com>
2020-01-17 16:32:07 -0500

    init

--- iOS_API/ViewController.swift
+++ iOS_API/ViewController.swift
@@ -10,34 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherLbl: UILabel!
+    @IBOutlet weak var cityTxt: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=617ad664055b1c7cebe17d578fa1116e")!
+  
+    }
     
-        let task =  URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                print(error)
-            }
-            else{
-                if let urlContent = data{
-                    do{
-                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
-                        print(jsonResult)
-                        //print(jsonResult["name"] as! String)
-                        
-                        if let description = ((jsonResult["weather"] as! NSArray)[0] as! NSDictionary)["description"] as? String
-                        { print(description)}
+    
+    @IBAction func selectCity(_ sender: UIButton) {
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=\(cityTxt.text!.replacingOccurrences(of: " ", with: "%20"))&appid=617ad664055b1c7cebe17d578fa1116e")!
+        
+            let task =  URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let error = error {
+                    print(error)
+                }
+                else{
+                    if let urlContent = data{
+                        do{
+                            let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+//                          print(jsonResult)
+                            //print(jsonResult["name"] as! String)
+                            
+                            if let description = ((jsonResult["weather"] as! NSArray)[0] as! NSDictionary)["description"] as? String
+                            {
+                                print(description)
+                                DispatchQueue.main.async {
+                                    self.weatherLbl.text=description
+                                }
+                            }
+                        }
+                        catch{
+                            print(error)
+                        }
+                       
                     }
-                    catch{
-                        print(error)
-                    }
-                   
                 }
             }
-        }
-        task.resume()
+            task.resume()
+        
     }
+    
+    
+    
 
 
 }
